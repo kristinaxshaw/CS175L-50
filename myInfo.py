caa8e051a9cb2c819e861f3007092a794af9501a
kristinaxshaw <91150764+kristinaxshaw@users.noreply.github.com>
2022-01-24 18:58:09 -0500

    myInfo Version 2

--- myInfo.py
+++ myInfo.py
@@ -2,4 +2,6 @@
 #Kristina Shaw
 #myInfo
 print("My name is Kristina Shaw.")
-print ("I am a student.") 
+print ("I live at 430 Libourel Road, Metuchen, New Jersey, 08840.")
+print ("My phone number is 908-668-9508.")
+print ("I am a Computer Science major.")
